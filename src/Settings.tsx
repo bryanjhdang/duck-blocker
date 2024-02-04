99415183288afc5088e68f7e02d2ae42c6a3c724
Bryan <bryandang10@gmail.com>
2024-02-04 00:17:25 -0800

    options page

---
+++ src/Settings.tsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+function Settings() {
+  return (
+    <>
+      <h1>Extension</h1>
+    </>
+  )
+}
+
+export default Settings
--- src/options/options.tsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-
-// const test = (
-//   <div>
-//     <h1>Options</h1>
-//     <p>Hello World</p>
-//   </div>
-// )
